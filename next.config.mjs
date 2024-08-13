3372befa950de9cea15b79f6d9e878fd8842bdb5
Yudhishthirr <officialyudhishthir@gmail.com>
2024-08-13 18:28:44 +0530

    add some next config headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//   async headers() {
-//     return [
-//         {
-//             // matching all API routes
-//             source: "/api/:path*",
-//             headers: [
-//                 { key: "Access-Control-Allow-Credentials", value: "true" },
-//                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-//                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-//                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//             ]
-//         }
-//     ]
-// },
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+},
     webpack: (
         config,
         { buildId, dev, isServer, defaultLoaders, webpack }
