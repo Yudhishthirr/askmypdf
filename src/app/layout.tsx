7d209dad7624679abff99cd683ccf802a2b918f7
Yudhishthir <165118848+Yudhishthirr@users.noreply.github.com>
2024-08-21 15:18:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chat with Your PDF - AI-Powered PDF Assistant",
-  image = "/chatpdf.png",
+  image: "/chatpdf.png",
   description:
     "Interact with your PDF documents using our AI-powered assistant. Upload, chat, and get instant insights from your PDFs with ease.",
   keywords: [
