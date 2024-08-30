fc939f0deadfeb98ecb8682c28ebcc1293899f1f
Yudhishthir <165118848+Yudhishthirr@users.noreply.github.com>
2024-08-30 12:19:00 +0530

    Update page.tsx auth call back change isloading

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,35 +1,36 @@
 "use client"
 
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 import { Loader2 } from 'lucide-react'
 import { trpc } from '../_trpc/clinet'
 
 const Page = () => {
   const router = useRouter()
-
-  // const searchParams = useSearchParams()
-  // const origin = searchParams.get('origin')
   const {data,isLoading} = trpc.authCallback.useQuery()
   
+
+  if(isLoading){
+    return (
+      <div className='w-full mt-24 flex justify-center'>
+        <div className='flex flex-col items-center gap-2'>
+          <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+          <h3 className='font-semibold text-xl'>
+            Setting up your account...
+          </h3>
+          <p>You will be redirected automatically.</p>
+        </div>
+      </div>
+    )
+  }
   if(data?.success == true){
     router.push('/dashboard')
-    //console.log("Your are register in mogodb know you are go to dashboard MESAAGE FROM AUTH-CALL-BACK")
+   
   }else{
     router.push('/api/auth/login?')
-    //console.log("you go to first kind login page becouse you are not in both in db or kind MESAAGE FROM AUTH-CALL-BACK")
+   
   }
-  return (
-    <div className='w-full mt-24 flex justify-center'>
-      <div className='flex flex-col items-center gap-2'>
-        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-        <h3 className='font-semibold text-xl'>
-          Setting up your account...
-        </h3>
-        <p>You will be redirected automatically.</p>
-      </div>
-    </div>
-  )
+  
 }
 
 export default Page
